413f9239e715e54003d9e69ba65991ad5c9470b0
reshuk-code <reshuksapkota2007@gmail.com>
2025-03-23 15:04:36 +0545

    Fixed mongoose issue in frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  rollupOptions: {
-    external: ["mongoose"], // Exclude mongoose from frontend
-  },
+  build: {
+    rollupOptions: {
+      external: ['mongoose'] // This ensures mongoose is not bundled
+    }
+  }
 })
